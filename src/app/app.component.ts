d6a62a1e82e9e656f0f450d380b51a8c65f6b286
Felipe Mendes Neves <felipemen74@gmail.com>
2025-07-06 22:18:14 -0300

    Configs iniciando projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  title = 'angular-portfolio';
+
 }
