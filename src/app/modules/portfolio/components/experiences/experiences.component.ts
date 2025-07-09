6c9458d42d6751c1ca886736b3ecd694b77707fd
Felipe Mendes Neves <felipemen74@gmail.com>
2025-07-08 22:42:09 -0300

    fix: ajustes no estilo do dialog e mudança no titulo.

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -11,10 +11,10 @@ export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
       summary: {
-        strong:"Estagiário de desenvolvimento web",
-        p:"IFNMG | Agosto/2024 - Março/2025"
+        strong:"Estagiário de Desenvolvimento Web",
+        p:"IFNMG - Januária | Agosto/2024 - Março/2025"
       },
-      text:"<p>Participei do projeto</p>",
+      text:"<p>Participação no projeto 'Adaptação do Sistema de Gerenciamento de Estágios' para o sistema acadêmico Cajuí, visando otimizar e adaptar funcionalidades para atender às demandas institucionais. Utilização de tecnologias como Yii2 (PHP), Docker e PostgreSQL para análise de requisitos, testes e implementação de melhorias, reduzindo o tempo de execução de tarefas administrativas como assinatura de documentos, validação de dados, e mudança de status do estágio. Colaboração com equipes multidisciplinares para garantir a eficiência e a usabilidade do sistema.</p>",
     }
   ])
 }
